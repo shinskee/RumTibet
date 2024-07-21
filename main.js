5065868f79b695240d1699c3560c7b6dafacd27b
Shinskee <elblack7777@gmail.com>
2024-07-21 17:23:17 +0800

    main

--- main.js
+++ main.js
@@ -21,12 +21,6 @@ clickMenu.forEach(element => {
 
 //** Programm swiper */
 
-// const swiper = new Swiper('.swiper', {
-//     direction: 'horizontal',
-//     spaceBetween: 20,
-//     slideClass: 'program__swiper-slide',
-// })
-
 const firstImage = document.querySelector('.program__swiper-slide-1')
 const secondImage = document.querySelector('.program__swiper-slide-2')
 
@@ -86,6 +80,133 @@ if(scrollTop) {
     }
   })
 }
+
+//** Scroll all page */
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !==1) {
+  let smoother = ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',    
+    smooth: 1,
+    effects: true,
+  })
+//** Навигация по меню */
+  const navLinks = document.querySelectorAll(".nav a")
+
+  let Linker = navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      Linker = link.getAttribute("href")
+      smoother.scrollTo(Linker, true, 'center center')
+    })
+  })
+//** Навигация по меню */
+//** Навигация ScrollTop */
+scrollTop.addEventListener('click', () => {
+  smoother.scrollTo('.header', true, 'center center')
+})
+//** Навигация ScrollTop */
+
+  gsap.from('.header__inner', {
+    opacity: 0,
+    duration: 1,
+    delay: 0.3
+  })
+
+  gsap.from('.hero__title', {
+    opacity: 0,
+    duration: 1,
+    delay: 0.4
+  })
+
+  gsap.from('.finder', {
+    opacity: 0,
+    duration: 1,
+    delay: 0.5
+  })
+
+  gsap.fromTo('.header', {opacity: 1}, {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: '.header',
+      start: 'center',
+      end: 'bottom',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.program', {opacity: 0, x: 500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.header',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.about', {opacity: 0, x: -500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.program',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.price', {opacity: 0, x: 500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.about',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.blog', {opacity: 0, x: -500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.price',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.blog-card blog__inner', {opacity: 0, x: 500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.price',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.photo', {opacity: 0, x: -500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.blog',
+      scrub: true,
+    }
+  })
+
+  gsap.fromTo('.form', {opacity: 0, x: 500}, {
+    opacity: 1, x: 0,
+    scrollTrigger: {
+      trigger: '.photo',
+      scrub: true,
+    }
+  })
+}
+
+
+
+
+
+  
+  
+
+
+
+
+
+  
+
+
 
 
 
